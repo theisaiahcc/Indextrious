5af478851e35fb298390a2472ce3bf087a23d266
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-07-11 09:54:42 -0700

    Added documentation to new classes/interfaces

--- Indextrious/Models/ICard.cs
+++ Indextrious/Models/ICard.cs
@@ -2,6 +2,9 @@
 {
     public interface ICard
     {
+        /// <summary>
+        /// Unique Integer identifier for card using the ICard interface
+        /// </summary>
         int Id { get; set; }
     }
 }
--- Indextrious/Models/IndexCard.cs
+++ Indextrious/Models/IndexCard.cs
@@ -3,7 +3,13 @@
     public class IndexCard : ICard
     {
         public int Id { get; set; }
+        /// <summary>
+        /// Stores an index cards title string
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// Stores an index cards body string
+        /// </summary>
         public string Body { get; set; }
     }
 }
