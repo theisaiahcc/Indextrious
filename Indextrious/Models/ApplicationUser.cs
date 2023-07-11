d7d015e3e64cb950f73bccc0c00175b19cc44d56
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-07-11 10:51:27 -0700

    Added application user class that contains list of collections

---
+++ Indextrious/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Indextrious.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        /// <summary>
+        /// Navigation property to the CardCollections owned by this user
+        /// </summary>
+        public List<CardCollection> RootCardFiles { get; set; } = new List<CardCollection>();
+    }
+}
