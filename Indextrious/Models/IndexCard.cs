54999c077617e2aaa8533f9b49bd7d3aa9942b2d
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-07-11 09:51:00 -0700

    Added index card class

---
+++ Indextrious/Models/IndexCard.cs
@@ -0,0 +1,9 @@
+﻿namespace Indextrious.Models
+{
+    public class IndexCard : ICard
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
