09c15effa2d907633884fd3d16fc67c883c1867c
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-07-13 11:45:03 -0700

    Submit now creates a collection in the database

---
+++ Indextrious/Controllers/CardCollectionController.cs
@@ -0,0 +1,48 @@
+﻿using Indextrious.Data;
+using Indextrious.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Indextrious.Controllers
+{
+    public class CardCollectionController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CardCollectionController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return BadRequest("Collection name cannot be empty.");
+            }
+
+            // Get the current user
+            var user = await _userManager.GetUserAsync(User);
+
+            var collection = new CardCollection
+            {
+                Name = name,
+                Owner = user
+            };
+
+            _context.Add(collection);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
--- Indextrious/wwwroot/js/site.js
+++ Indextrious/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#create-collection-form').on('submit', function (e) {
+        e.preventDefault();  // prevent the form from doing a full postback
+
+        $.ajax({
+            type: "POST",
+            url: $(this).attr('action'), // submit to the Create action
+            data: $(this).serialize(),  // get form data
+            success: function (data) {
+                refreshCollections();
+                // close the modal
+                $('#myModal').modal('hide');
+            },
+            error: function () {
+                // show an error message
+                alert('An error occurred. Please try again.');
+            }
+        });
+    });
+});
