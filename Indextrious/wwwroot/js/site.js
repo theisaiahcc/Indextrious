4e12de190213bc4511b15f099a19a3ef4592caea
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-07-18 10:47:20 -0700

    Created UI to view and navigate CardCollections

--- Indextrious/wwwroot/js/site.js
+++ Indextrious/wwwroot/js/site.js
@@ -1,7 +1,36 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Code from Hyperplexed YouTube channel
+const track = document.getElementById("collection-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, { duration: 1200, fill: "forwards" });
+}
+
+
+
 $(document).ready(function () {
     $('#create-collection-form').on('submit', function (e) {
         e.preventDefault();  // prevent the form from doing a full postback
@@ -22,3 +51,4 @@ $(document).ready(function () {
         });
     });
 });
+
